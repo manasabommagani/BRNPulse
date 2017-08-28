bff13d8eb772034fb38935bbc7c13dded3a26195
student <student@Achievers-iMac.local>
2017-08-28 11:58:19 +0530

    Initial Commit

---
+++ BRNPulse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRNPulse
+//
+//  Created by student on 8/28/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BRNPulse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRNPulse
+//
+//  Created by student on 8/28/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
